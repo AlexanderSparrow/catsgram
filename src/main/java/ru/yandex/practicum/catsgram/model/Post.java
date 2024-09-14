139bc8f3d152cf982e8afa8d15f98b3e476bb33e
Alexander Gorobets <for.gorobets@yandex.ru>
2024-09-14 17:58:25 +0300

    Финальное задание 10го спринта. Создаем cкрепный аналог запрещеннграмма, только для котиков:)
    Добавлены некоторые контроллеры и обработчики исключений

--- src/main/java/ru/yandex/practicum/catsgram/model/Post.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -1,8 +1,9 @@
 package ru.yandex.practicum.catsgram.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NonNull;
 
 import java.time.Instant;
 
@@ -11,8 +12,19 @@ import java.time.Instant;
 public class Post {
     Long id;
     long authorId;
-    @NonNull
     String description;
-    @NonNull
     Instant postDate;
+
+    // Конструктор с аннотацией @JsonCreator для корректной десериализации
+    @JsonCreator
+    public Post(@JsonProperty("authorId") long authorId,
+                @JsonProperty("description") String description) {
+        this.authorId = authorId;
+        this.description = description;
+        this.postDate = Instant.now(); // Можно установить текущее время, если необходимо
+    }
+
+    // Конструктор по умолчанию (необходим для сериализации/десериализации)
+    public Post() {
+    }
 }
